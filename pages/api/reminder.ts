1845c57ad811fcc8219623c75bcfbb949e995621
Matt <info@mgates.me>
2021-07-07 11:05:37 -0500

    Only loop groups with members

--- pages/api/reminder.ts
+++ pages/api/reminder.ts
@@ -1,23 +1,28 @@
-import { NowRequest, NowResponse } from "@vercel/node";
-import { connectToDatabase } from "./_connectToDatabase";
-import { sendMsg, StandupGroup, Member, About } from "./_helpers";
+import { NowRequest, NowResponse } from '@vercel/node';
+import { connectToDatabase } from './_connectToDatabase';
+import { sendMsg, StandupGroup, Member, About } from './_helpers';
 
 module.exports = async (req: NowRequest, res: NowResponse) => {
   const db = await connectToDatabase();
-  const groups = await db.collection("groups").find({}).toArray();
+  const groups = await db.collection('groups').find({}).toArray();
 
-
-  const reminders = []
-  groups.forEach((group: StandupGroup) => {
-    group.members.forEach((member: Member) => {
-      if (member.submitted === false) {
-        reminders.push(sendMsg("Reminder, please submit an update. Updates are due by 11 AM EST", member.about.id))
-      }
+  const reminders = [];
+  groups
+    .filter((g) => !!g.members.length)
+    .forEach((group: StandupGroup) => {
+      group.members.forEach((member: Member) => {
+        if (member.submitted === false) {
+          reminders.push(
+            sendMsg(
+              'Reminder, please submit an update. Updates are due by 11 AM EST',
+              member.about.id
+            )
+          );
+        }
+      });
     });
-  });
 
-  await Promise.all(reminders)
+  await Promise.all(reminders);
 
   res.json({ status: 200 });
-
 };
--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,39 +1,44 @@
-import { NowRequest, NowResponse } from "@vercel/node";
-import { connectToDatabase } from "./_connectToDatabase";
-import { sendMsg, StandupGroup, Member, About } from "./_helpers";
-
-
+import { NowRequest, NowResponse } from '@vercel/node';
+import { connectToDatabase } from './_connectToDatabase';
+import { sendMsg, StandupGroup, Member, About } from './_helpers';
 
 module.exports = async (req: NowRequest, res: NowResponse) => {
-
-    
   const db = await connectToDatabase();
 
-    const addUpdate = async () => { await db.collection("groups").updateMany(
-    {  },
-    {
-      $set: {
-        "members.$[elem].submitted": false,
-        "members.$[elem].update": '',
+  const addUpdate = async () => {
+    await db.collection('groups').updateMany(
+      {},
+      {
+        $set: {
+          'members.$[elem].submitted': false,
+          'members.$[elem].update': '',
+        },
       },
-    },
-    { arrayFilters: [{ "elem.submitted": true}] }
-  );
-}
-  const groups = await db.collection("groups").find({}).toArray();
+      { arrayFilters: [{ 'elem.submitted': true }] }
+    );
+  };
+  const groups = await db.collection('groups').find({}).toArray();
 
   const sentStandup = [];
-  groups.forEach((group: StandupGroup) => {
-    group.members.forEach((member: Member) => {
-      sentStandup.push(
-        sendMsg(member.about.first_name + " (@" + member.about.username + "): \n" + member.update, group.chatId)
-      );
-
+  groups
+    .filter((g) => !!g.members.length)
+    .forEach((group: StandupGroup) => {
+      group.members.forEach((member: Member) => {
+        sentStandup.push(
+          sendMsg(
+            member.about.first_name +
+              ' (@' +
+              member.about.username +
+              '): \n' +
+              member.update,
+            group.chatId
+          )
+        );
+      });
     });
-  });
 
   await Promise.all(sentStandup);
-  await addUpdate()
+  await addUpdate();
 
   res.json({ status: 200 });
 };
