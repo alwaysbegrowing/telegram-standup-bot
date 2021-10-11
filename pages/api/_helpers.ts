ce4277f48d0a0ca982b36f9e49152920ae5ffe79
Matt <info@mgates.me>
2021-10-10 22:18:29 -0500

    fix json

--- pages/api/_helpers.ts
+++ pages/api/_helpers.ts
@@ -140,9 +140,7 @@ export const sendMsg = async (
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
-  })
-    .then((res) => res.json())
-    .then((r) => console.log(r));
+  });
 };
 
 const secret = createHash('sha256')
