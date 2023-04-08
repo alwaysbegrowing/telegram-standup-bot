edb13724f290eb65b856e3dc47961fdef85c5511
Matt <Geczy@users.noreply.github.com>
2023-04-08 08:20:29 -0500

    chore: refactor mongodb connection

--- pages/api/lib/_connectToDatabase.ts
+++ pages/api/lib/_connectToDatabase.ts
@@ -1,37 +1,31 @@
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
-let cached = global.mongo;
+// Maintain a cached connection across hot reloads in development.
+let cachedConnection = globalThis.mongo;
 
-if (!cached) {
-  cached = global.mongo = { conn: null, promise: null };
+if (!cachedConnection) {
+  cachedConnection = globalThis.mongo = { conn: null, promise: null };
 }
 
 export async function connectToDatabase() {
-  if (cached.conn) {
-    return cached.conn;
+  if (cachedConnection.conn) {
+    return cachedConnection.conn;
   }
 
-  if (!cached.promise) {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+  if (!cachedConnection.promise) {
+    const options = {
+      // Other MongoClient options can be added here, if needed
     };
 
-    cached.promise = MongoClient.connect(
+    cachedConnection.promise = MongoClient.connect(
       process.env.MONGODB_FULL_URI,
-      opts
-    ).then((client) => {
-      return {
-        client,
-        db: client.db(process.env.MONGODB_NAME),
-      };
-    });
+      options
+    ).then((client) => ({
+      client,
+      db: client.db(process.env.MONGODB_NAME),
+    }));
   }
-  cached.conn = await cached.promise;
-  return cached.conn;
+
+  cachedConnection.conn = await cachedConnection.promise;
+  return cachedConnection.conn;
 }
