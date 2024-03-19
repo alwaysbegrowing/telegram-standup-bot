5c605f0de26edd3a4dd27e9b9235d6d42bcc009f
Matt Gates <Geczy@users.noreply.github.com>
2024-03-19 08:47:41 -0500

    Revert "bots cant send reactions"
    This reverts commit 104dd441744edee604e46368de9d380cbe707b42.

--- pages/api/lib/_helpers.ts
+++ pages/api/lib/_helpers.ts
@@ -1,8 +1,116 @@
+import BigInt from 'big-integer';
 import { createHash, createHmac } from 'crypto';
 import memoize from 'fast-memoize';
 import client from './_client';
 import { ANONYMOUS } from './_locale.en';
 import { Member } from './_types';
+import { Api } from 'telegram';
+
+export const CHANNEL_ID_LENGTH = 14; // 14 symbols, including -100 prefix
+const LEGACY_CHANNEL_ID_MIN_LENGTH = 11; // Example: -1234567890
+
+function checkIfChannelId(id: string) {
+  if (id.length >= CHANNEL_ID_LENGTH) return id.startsWith('-100');
+  // LEGACY Unprefixed channel id
+  if (id.length === LEGACY_CHANNEL_ID_MIN_LENGTH && id.startsWith('-4'))
+    return false;
+  return id.length >= LEGACY_CHANNEL_ID_MIN_LENGTH;
+}
+
+export function getEntityTypeById(chatOrUserId: string) {
+  if (!chatOrUserId.startsWith('-')) {
+    return 'user';
+  } else if (checkIfChannelId(chatOrUserId)) {
+    return 'channel';
+  } else {
+    return 'chat';
+  }
+}
+export function buildMtpPeerId(id: string, type: 'user' | 'chat' | 'channel') {
+  if (type === 'user') {
+    return BigInt(id);
+  }
+
+  if (type === 'channel') {
+    if (id.length === CHANNEL_ID_LENGTH) {
+      return BigInt(id.slice(4));
+    }
+
+    // LEGACY Unprefixed channel id
+    return BigInt(id.slice(1));
+  }
+
+  return BigInt(id.slice(1));
+}
+
+export function buildInputPeer(
+  chatOrUserId: string,
+  accessHash?: string,
+): Api.TypeInputPeer {
+  const type = getEntityTypeById(chatOrUserId);
+
+  if (type === 'user') {
+    return new Api.InputPeerUser({
+      userId: buildMtpPeerId(chatOrUserId, 'user'),
+      accessHash: BigInt(accessHash!),
+    });
+  } else if (type === 'channel') {
+    return new Api.InputPeerChannel({
+      channelId: buildMtpPeerId(chatOrUserId, 'channel'),
+      accessHash: BigInt(accessHash!),
+    });
+  } else {
+    return new Api.InputPeerChat({
+      chatId: buildMtpPeerId(chatOrUserId, 'chat'),
+    });
+  }
+}
+export type ApiReaction = Api.ReactionEmoji | Api.ReactionCustomEmoji;
+
+export function buildInputReaction(reaction?: ApiReaction) {
+  if (reaction && 'emoticon' in reaction) {
+    return new Api.ReactionEmoji({
+      emoticon: reaction.emoticon,
+    });
+  }
+
+  if (reaction && 'documentId' in reaction) {
+    return new Api.ReactionCustomEmoji({
+      documentId: BigInt(reaction.documentId),
+    });
+  }
+
+  return new Api.ReactionEmpty();
+}
+export async function sendReaction({
+  chat,
+  messageId,
+  reactions,
+  shouldAddToRecent,
+}: {
+  chat: { id: string; accessHash?: string };
+  messageId: number;
+  reactions?: ApiReaction[];
+  shouldAddToRecent?: boolean;
+}) {
+  if (!client.connected) await client.connect();
+
+  try {
+    await client.invoke(
+      new Api.messages.SendReaction({
+        reaction: reactions?.map((r) => buildInputReaction(r)),
+        peer: buildInputPeer(chat.id, chat.accessHash),
+        msgId: messageId,
+        ...(shouldAddToRecent && { addToRecent: true }),
+      }),
+    );
+  } catch (error) {
+    console.error('Error sending reaction:', error);
+    return { status: 500 };
+  }
+
+  return { status: 200 };
+}
 
 export const sendMsg = async (
   postfix: string,
--- pages/api/lib/command-helpers/_addToStandupGroup.ts
+++ pages/api/lib/command-helpers/_addToStandupGroup.ts
@@ -1,5 +1,5 @@
 import { connectToDatabase } from '../_connectToDatabase';
-import { sendMsg } from '../_helpers';
+import { sendMsg, sendReaction } from '../_helpers';
 import { ALREADY_SUBSCRIBED_MESSAGE, SUBSCRIBED_MESSAGE } from '../_locale.en';
 import { About, Member, StandupGroup } from '../_types';
 
@@ -17,8 +17,16 @@ export const addToStandupGroup = async (
     'groups.chatId': chatId,
   });
 
+  console.log('adding');
+
   if (userExistsInGroup) {
-    return await sendMsg(ALREADY_SUBSCRIBED_MESSAGE, chatId, messageId);
+    console.log({
+      chatId,
+      messageId,
+      userId,
+    });
+    return await sendReaction({ chat: { id: `${chatId}` }, messageId });
+    // return await sendMsg(ALREADY_SUBSCRIBED_MESSAGE, chatId, messageId);
   }
 
   const group: StandupGroup = {
